411234cc3aa1180b9afcbf367f3ce1a670c2df15
Marcin Lulek <info@webreactor.eu>
2019-09-29 21:09:38 +0200

    server

--- cmd/serve.go
+++ cmd/serve.go
@@ -2,8 +2,9 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"net/http"
+	"path/filepath"
 )
 
 // serveCmd represents the serve command
@@ -11,9 +12,7 @@ var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Serves the output directory for presentation",
 	Long:  ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("serve called")
-	},
+	Run:   servePresentation,
 }
 
 func init() {
@@ -26,4 +25,16 @@ func init() {
 type ConfigServer struct {
 	OutputDir string
 	Port      int
+}
+
+func servePresentation(cmd *cobra.Command, args []string) {
+	config := ConfigServer{}
+	config.Port, _ = cmd.Flags().GetInt("port")
+	config.OutputDir, _ = cmd.Flags().GetString("output-dir")
+	config.OutputDir, _ = filepath.Abs(config.OutputDir)
+	fmt.Printf("Static directory: %s \nserving: http://127.0.0.1:%d \n", config.OutputDir, config.Port)
+
+	fs := http.FileServer(http.Dir(config.OutputDir))
+	http.Handle("/", fs)
+	http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
 }
